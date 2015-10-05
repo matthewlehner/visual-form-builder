f5a04dfcc20ea90843468b3cee9f2df76ede16c9
Matthew Lehner <matthew@mpl.io>
2015-10-05 10:52:42 -0700

    move stuff into src dir.

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,17 @@
+var ExtractTextPlugin = require("extract-text-webpack-plugin");
+var path = require('path');
+
 module.exports = {
-  entry: "./index",
+  entry: ["./src/css/index.scss", "./src/index.js"],
 
   output: {
     path: __dirname,
     filename: "bundle.js",
     publicPath: "/static/"
+  },
+
+  resolve: {
+    root: [__dirname + "/src"]
   },
 
   module: {
@@ -13,6 +20,16 @@ module.exports = {
       exclude: /node_modules/,
       loaders: ["babel"],
       include: __dirname
+    }, {
+      test: /\.scss$/,
+      loader: ExtractTextPlugin.extract(
+        "style-loader", "css-loader", "autoprefixer-loader",
+        "sass-loader?includePaths[]=" + (path.resolve(__dirname, "./node_modules"))
+      )
     }]
-  }
+  },
+
+  plugins: [
+    new ExtractTextPlugin("./bundle.css")
+  ]
 };
