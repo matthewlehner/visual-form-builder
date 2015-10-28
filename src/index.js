bb851903801924e87e26e960c4412fbdcf4d6b52
Matthew Lehner <matthew@mpl.io>
2015-10-28 11:19:54 -0700

    update react and redux functions.

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { render } from "react-dom";
 import { Provider } from "react-redux";
 import App from "./containers/App";
 import configureStore from "./store/configureStore";
@@ -8,10 +9,10 @@ import { DevTools, DebugPanel, LogMonitor } from "redux-devtools/lib/react";
 
 const store = configureStore();
 
-React.render(
+render(
   <div>
     <Provider store={store}>
-      {() => <App />}
+      <App />
     </Provider>
     <DebugPanel top right bottom>
       <DevTools store={store} monitor={LogMonitor} />
