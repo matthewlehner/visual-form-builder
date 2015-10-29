7e16c679db17807e4055d1488838554f907ffeb7
Matthew Lehner <matthew@mpl.io>
2015-10-28 19:21:21 -0700

    move non-formbuilder elements to app component.

--- src/components/FormBuilder.js
+++ src/components/FormBuilder.js
@@ -14,15 +14,10 @@ const inputTypes = [
 export default class FormBuilder extends Component {
   render() {
     return (
-      <div className="visual-form-builder">
-        <header>
-          <h1>I'm a FormBuilder!</h1>
-        </header>
-        <section className="visual-form-builder__body">
-          <ComposedForm />
-          <InputTypeList inputTypes={inputTypes} />
-        </section>
-      </div>
+      <section className="visual-form-builder__body">
+        <ComposedForm />
+        <InputTypeList inputTypes={inputTypes} />
+      </section>
     );
   }
 }
--- src/containers/App.js
+++ src/containers/App.js
@@ -1,3 +1,15 @@
+import React, { Component } from "react";
 import FormBuilder from "../components/FormBuilder";
 
-export default FormBuilder;
+export default class App extends Component {
+  render() {
+    return (
+      <div className="visual-form-builder">
+        <header>
+          <h1>I'm a FormBuilder!</h1>
+        </header>
+        <FormBuilder />
+      </div>
+    );
+  }
+}
